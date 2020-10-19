38650dd7139d23e67f6c1504866476464943596f
Ricardo Alvares <ricardo.alvares@xing.com>
2020-10-19 22:40:13 +0100

    1st commit

---
+++ config/webpack/environment.js
@@ -0,0 +1,12 @@
+$(document).on('turbolinks:load', function () {
+  alert("We made it!");
+})
+
+const webpack = require('webpack')
+environment.plugins.prepend('Provide',
+  new webpack.ProvidePlugin({
+    $: 'jquery/src/jquery',
+    jQuery: 'jquery/src/jquery'
+  })
+)
+module.exports = environment
